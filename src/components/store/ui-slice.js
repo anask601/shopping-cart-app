d7899e374b6e360bfcb972f75c65d078e9f458b5
anask601 <anask60155@gmail.com>
2022-03-11 15:02:47 +0530

    remove comments

--- src/components/store/ui-slice.js
+++ src/components/store/ui-slice.js
@@ -12,11 +12,3 @@ const uiSlice = createSlice({
 
 export const uiActions = uiSlice.actions;
 export default uiSlice;
-
-// for (let i = 100; i <= 2000; i++) {
-//   if (i % 4 === 0 && i % 100 !== 0) {
-//     console.log(`it's a leep year ${i}`);
-//   } else if (i % 400 === 0) {
-//     console.log(`it's a leep year ${i}`);
-//   }
-// }
